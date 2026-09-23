9a719442ab1d95c5d03e72d74322fd7ad28f3c20
HYUNJEONG LEE <109647353+howareyou2@users.noreply.github.com>
2026-09-23 23:01:33 +0900

    [Passed] 최대로 겹치는 지점 (129ms, 10MB)

---
+++ trail2/최대로 겹치는 지점/maximum-overlapped-points.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] start = new int[n];
+        int[] end = new int[n];
+        for (int i = 0; i < n; i++) {
+            start[i] = sc.nextInt();
+            end[i] = sc.nextInt();
+        }
+        // Please write your code here.
+
+        int[] points = new int[101];
+        for (int i = 0; i < n; i++) {
+            for(int x = start[i]; x <= end[i]; x++){
+                points[x] += 1;
+            }
+        }
+
+        int result = Integer.MIN_VALUE;
+        for(int count : points){
+            result = Math.max(result, count);
+        }
+        System.out.print(result);
+    }
+}
